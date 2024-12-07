1783a6c2dd3bc28c349f53996977f5b16b6d2e00
erfan <erfanghezi5050@gmail.com>
2024-12-07 19:17:49 +0330

    Update this component

--- src/components/indexComponents/Mobiles/Mobile.jsx
+++ src/components/indexComponents/Mobiles/Mobile.jsx
@@ -13,11 +13,7 @@ export default function Mobile() {
       <div className="flex flex-col lg:flex-row items-center lg:mt-2 xl:mt-16 justify-between w-[100%] h-full">
         <div className="w-full hidden lg:block lg:w-[20%] xl:w-[25%] h-[40rem] lg:h-[38rem] xl:h-[33rem] rounded-3xl overflow-hidden shadow-[#DF2648] shadow-xl">
           <div className="w-[100%] h-[100%]">
-            <img
-              src="/images/mobile/Mobile.jpg"
-              className="w-[100%] h-[100%]"
-              alt=""
-            />
+            <img src="/images/mobile/Mobile.jpg" className="w-[100%] h-[100%]" alt="" />
           </div>
         </div>
         <div className="w-[100%] h-[35rem] lg:h-[auto] xl:w-[70%] mx-auto flex items-center justify-between">
