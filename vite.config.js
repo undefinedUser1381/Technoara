730df2e4895fa9f0a74d45e620c6823870c1c0fc
erfan <erfanghezi5050@gmail.com>
2024-12-17 22:31:59 +0330

    gh pages added

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : "/technoara/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
