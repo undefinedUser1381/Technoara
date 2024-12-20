3068d541b3d46861e052c0a8a76f8a263eed1c06
erfan <erfanghezi5050@gmail.com>
2024-12-20 11:48:56 +0330

    Update src

--- src/components/sharedComponents/ProductsWithNoDiscounts/ProductsWithNoDiscounts.jsx
+++ src/components/sharedComponents/ProductsWithNoDiscounts/ProductsWithNoDiscounts.jsx
@@ -1,11 +1,12 @@
 import React, { memo } from 'react'
+import ProductSrc from "/public/images/products/1-10.jpg"
 
 const ProductsWithNoDiscounts = memo(() => {
   console.log("ProductsWithNoDiscounts Rendered");
   return (
       <div className='flex relative flex-col w-[100%] h-[32rem] xs:h-[29rem] sm:h-[28rem] md:h-[29rem] gap-2 shadow-lg transition-all hover:bg-[#EFF0F5] p-7 rounded-lg'>
        <div className='w-[14rem] mx-auto h-[14rem]'>
-        <img src="/images/products/1-10.jpg" className='w-[100%] h-[100%]' alt="" />
+        <img src={ProductSrc} className='w-[100%] h-[100%]' alt="" />
          <div className='absolute top-5 right-5'>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-7">
           <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
