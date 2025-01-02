53a72660327da21b8bf395201c724002a71bf604
erfan <erfanghezi5050@gmail.com>
2025-01-03 00:16:35 +0330

    Edited Table of products

--- src/components/panelComponents/PanelProducts/ProductsTable/ProductsTable.jsx
+++ src/components/panelComponents/PanelProducts/ProductsTable/ProductsTable.jsx
@@ -15,8 +15,8 @@ export default function ProductsTable() {
    ]
 
   return (
-    <div className='rounded-2xl border dark:border-gray-700'>
-       <Table className="overflow-scroll lg:overflow-hidden rounded-2xl">
+    <div className='rounded-2xl overflow-hidden border dark:border-gray-500'>
+       <Table className="overflow-scroll lg:overflow-hidden dark:border-gray-700 rounded-2xl">
        <TableHeader className="bg-white text-black dark:bg-[#1B2631] dark:text-white">
       <TableRow className="font-Kalame text-[0.9rem] dark:border-gray-600 *:p-4 lg:text-[1rem]">
         <TableHead className="text-center">ردیف</TableHead>
