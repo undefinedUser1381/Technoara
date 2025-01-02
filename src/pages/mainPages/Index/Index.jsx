37d5219402b440fe73ea92de159a7b0c99015ae1
erfan <erfanghezi5050@gmail.com>
2025-01-03 00:18:23 +0330

    Updated this comp

--- src/pages/mainPages/Index/Index.jsx
+++ src/pages/mainPages/Index/Index.jsx
@@ -1,41 +1,37 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useEffect } from "react";
 import Header from "../../../components/sharedComponents/Header/Header";
 import Footer from "../../../components/sharedComponents/Footer/Footer";
-import CategoryBox from '../../../components/indexComponents/CategoryBox/CategoryBox'
-import PopularCategory from '../../../components/indexComponents/PopularCategory/PopularCategory'
-import NewProducts from '../../../components/indexComponents/NewProducts/NewProducts'
-import Mobile from '../../../components/indexComponents/Mobiles/Mobile'
-import BestSellingProducts from '../../../components/indexComponents/BestSellingProducts/BestSellingProducts'
-import LapTop from '../../../components/indexComponents/LapTops/LapTop'
-import Articles from '../../../components/indexComponents/Articles/Articles'
-import DiscountCarts from '../../../components/indexComponents/DiscountCarts/DiscountCarts'
-import Banner from "../../../components/indexComponents/Banner/Banner"
-import { AuthContext } from '@/context/AuthContext/AuthContext';
-import useDocumentTitle from '@/hooks/useDocumentTitle';
+import CategoryBox from "../../../components/indexComponents/CategoryBox/CategoryBox";
+import PopularCategory from "../../../components/indexComponents/PopularCategory/PopularCategory";
+import NewProducts from "../../../components/indexComponents/NewProducts/NewProducts";
+import Mobile from "../../../components/indexComponents/Mobiles/Mobile";
+import BestSellingProducts from "../../../components/indexComponents/BestSellingProducts/BestSellingProducts";
+import LapTop from "../../../components/indexComponents/LapTops/LapTop";
+import Articles from "../../../components/indexComponents/Articles/Articles";
+import DiscountCarts from "../../../components/indexComponents/DiscountCarts/DiscountCarts";
+import Banner from "../../../components/indexComponents/Banner/Banner";
+import useDocumentTitle from "@/hooks/useDocumentTitle";
 
 export default function Index() {
-
-  const auth = useContext(AuthContext);
-  
-  useDocumentTitle("تکنوآرا | صفحه اصلی")
+  useDocumentTitle("تکنوآرا | صفحه اصلی");
 
   return (
     <>
-     <Header/>
-      <main className='mt-10 px-1 w-full py-4'>
-        <Banner/>
-        <div className='flex flex-col xl:flex-row mt-10 items-start h-auto xl:h-[25.5rem] justify-between'>
-          <DiscountCarts/>
-          <CategoryBox/>
+      <Header />
+      <main className="mt-10 px-1 w-full py-4">
+        <Banner />
+        <div className="flex flex-col xl:flex-row mt-10 items-start h-auto xl:h-[25.5rem] justify-between">
+          <DiscountCarts />
+          <CategoryBox />
         </div>
-        <PopularCategory/>
+        <PopularCategory />
         <NewProducts />
-        <Mobile/>
-        <BestSellingProducts/>
-        <LapTop/>
-        <Articles/>
+        <Mobile />
+        <BestSellingProducts />
+        <LapTop />
+        <Articles />
       </main>
       <Footer />
     </>
-  )
+  );
 }
