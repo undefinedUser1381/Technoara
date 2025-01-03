7a7ea19e64799d0970f0e150c8d08d7753ebe36e
erfan <erfanghezi5050@gmail.com>
2025-01-03 12:07:24 +0330

    Add variant function

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode : ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class"],
   theme: {
-  	extend: {
-  		fontFamily: {
-  			'Dorna': 'Dorna',
-  			'YekanBakh': 'YekanBakh',
-  			'Kalame': 'Kalame'
-  		},
-  		colors: {
-  			'primary': '#AF47D2',
-  			'secondary': '#021959',
-  		},
-  		boxShadow: {
-  			'custom': '0px -4px 18px 0px #0000001c'
-  		},
-  		screens: {
-  			'xs': '368px',
-  			'sm': '576px'
-  		},
-  		keyframes: {
-  			showTab: {
-  				'0%': {
-  					opacity: '0'
-  				},
-  				'100%': {
-  					opacity: '1'
-  				}
-  			}
-  		},
-  		animation: {
-  			showTab: 'showTab 0.3s ease-in-out'
-  		}
-  	}
+    extend: {
+      fontFamily: {
+        Dorna: "Dorna",
+        YekanBakh: "YekanBakh",
+        Kalame: "Kalame",
+      },
+      colors: {
+        primary: "#AF47D2",
+        secondary: "#021959",
+      },
+      boxShadow: {
+        custom: "0px -4px 18px 0px #0000001c",
+      },
+      screens: {
+        xs: "368px",
+        sm: "576px",
+      },
+      keyframes: {
+        showTab: {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+      },
+      animation: {
+        showTab: "showTab 0.3s ease-in-out",
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
-}
+  plugins: [
+    require("tailwindcss-animate"),
+    function ({ addVariant }) {
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+    },
+  ],
+};
