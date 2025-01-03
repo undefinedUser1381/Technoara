c834b14d260b6fe204ec725d92cdb0f458e7a2e4
erfan <erfanghezi5050@gmail.com>
2025-01-03 13:16:12 +0330

    Add product form

--- src/components/panelComponents/PanelProducts/PanelProducts.jsx
+++ src/components/panelComponents/PanelProducts/PanelProducts.jsx
@@ -1,13 +1,9 @@
-import React, { useState } from 'react'
 import PagesTitle from '../PagesTitle/PagesTitle'
 import Panelboxes from '../PanelBoxes/Panelboxes'
 import ProductsTable from './ProductsTable/ProductsTable'
-import ProductEdit from './ProductEdit/ProductEdit'
+import ProductAddForm from './َProductAddForm/َProductAddForm'
 
 export default function PanelProducts() {
-
-  const [formStatus , setFormStatus] = useState("اضافه محصول")
-
   return (
     <div className='w-[100%] min-h-screen bg-[#F9FAFB] p-0 lg:p-4 dark:bg-[#111827]'>
      <div className='mt-[7rem] w-full mr-auto lg:w-[80%] px-4 text-white'>
@@ -44,7 +40,6 @@ export default function PanelProducts() {
          <Panelboxes>
            <div className='w-16 h-16 bg-red-700 rounded-full flex items-center justify-center'> 
            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 text-red-300"><path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" /></svg>
-
            </div>
            <div className='flex font-Kalame flex-col gap-2'>
             <span className='text-black dark:text-[#BFBFD6]'>محصول رایگان</span>
@@ -66,7 +61,7 @@ export default function PanelProducts() {
        </div>
        <div className='mt-9 bg-white dark:bg-[#1B2631] p-5 rounded-lg'>
          <div className='flex items-center justify-between'>
-           <h4 className='font-Kalame w-full text-[1.1rem] relative text-black pr-8 dark:text-white'> فرم ویرایش و اضافه محصول
+           <h4 className='font-Kalame w-full text-[1.1rem] relative text-black pr-8 dark:text-white'> فرم اضافه محصول
             <div className='bg-blue-600 w-5 h-5 rounded-full absolute top-1 right-0'></div>
            </h4>
          </div>
@@ -74,7 +69,7 @@ export default function PanelProducts() {
               <span onClick={(e) => setFormStatus(e.target.innerHTML)} className='font-Kalame dark:text-white bg-blue-600 p-1 rounded-tr-xl rounded-br-xl py-2 cursor-pointer border-l-8 border-white dark:border-[#1B2631] px-3'>ادیت محصول</span>
               <span onClick={(e) => setFormStatus(e.target.innerHTML)} className='font-Kalame dark:text-white bg-blue-600 p-1 rounded-tl-xl rounded-bl-xl px-3 py-2 cursor-pointer'>اضافه محصول</span>
            </div>
-         <ProductEdit submit={formStatus}/>
+         <ProductAddForm />
        </div>
      </div>
     </div>
