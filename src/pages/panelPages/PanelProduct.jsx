7fa0025829133a57fc42cd59d6859b5d24dc746b
erfan <erfanghezi5050@gmail.com>
2025-01-04 16:30:17 +0330

    Add title to page

--- src/pages/panelPages/PanelProduct.jsx
+++ src/pages/panelPages/PanelProduct.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import PanelProducts from '@/components/panelComponents/PanelProducts/PanelProducts'
+import useDocumentTitle from '@/hooks/useDocumentTitle'
 
 export default function PanelProduct() {
+
+  useDocumentTitle("تکنوآرا | محصولات")
+   
   return (
     <><PanelProducts /></>
   )
