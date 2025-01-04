475e4e94e86f2d311d4309395445e3dbc2aa930a
erfan <erfanghezi5050@gmail.com>
2025-01-04 16:31:03 +0330

    Add title to page

--- src/pages/mainPages/Register/RegisterPage.jsx
+++ src/pages/mainPages/Register/RegisterPage.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Register from '../../../components/allComponents/Auth/Register/Register'
+import useDocumentTitle from '@/hooks/useDocumentTitle'
 
 export default function RegisterPage() {
+
+  useDocumentTitle("تکنو آرا | ثبت نام")
+
   return (
      <Register />
   )
