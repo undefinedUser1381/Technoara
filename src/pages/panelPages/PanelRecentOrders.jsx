68921acf23b5910e6c9f70af2b7fdd4b5222735c
erfan <erfanghezi5050@gmail.com>
2025-01-04 16:29:59 +0330

    Add title to page

--- src/pages/panelPages/PanelRecentOrders.jsx
+++ src/pages/panelPages/PanelRecentOrders.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import PanelRecentOrder from '../../components/panelComponents/PanelRecentOrder/PanelRecentOrder'
+import useDocumentTitle from '@/hooks/useDocumentTitle'
 
 export default function PanelRecentOrders() {
+
+  useDocumentTitle("تکنوآرا | سفارشات")
+
+
   return (
     <>
      <PanelRecentOrder />
