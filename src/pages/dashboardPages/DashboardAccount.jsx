b966802faece1430ab2112b5a97dadc07d8829dd
erfan <erfanghezi5050@gmail.com>
2025-01-04 16:26:05 +0330

    Add title to page

--- src/pages/dashboardPages/DashboardAccount.jsx
+++ src/pages/dashboardPages/DashboardAccount.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import AccountInfo from '../../components/dashboardComponents/AccountInfo/AccountInfo'
+import useDocumentTitle from '@/hooks/useDocumentTitle'
 
 export default function DashboardAccount() {
+
+  useDocumentTitle("تکنوآرا | اطلاعات حساب")
+
   return (
     <>
      <AccountInfo />
