16a8940323516761096768eb66d39968905db0fe
erfan <erfanghezi5050@gmail.com>
2025-01-04 16:28:25 +0330

    Add title to page

--- src/pages/panelPages/PanelUserDetails.jsx
+++ src/pages/panelPages/PanelUserDetails.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import PanelUserDetail from '@/components/panelComponents/PanelUserDetail/PanelUserDetail'
+import useDocumentTitle from '@/hooks/useDocumentTitle'
 
 export default function PanelUserDetails() {
+
+  useDocumentTitle("تکنوآرا | جزییات کاربر")
+
   return (
     <><PanelUserDetail /></>
   )
