e7ab0a2c62bbfa57cc6ce21d06ad6f1c8dd444ec
erfan <erfanghezi5050@gmail.com>
2025-01-04 16:27:19 +0330

    Add title to page

--- src/pages/dashboardPages/DashboardOrders.jsx
+++ src/pages/dashboardPages/DashboardOrders.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Orders from "../../components/dashboardComponents/Orders/Orders";
+import useDocumentTitle from '@/hooks/useDocumentTitle';
 
 export default function DashboardOrders() {
+
+  useDocumentTitle("تکنوآرا | سفارشات")
 
   return (
     <> 
