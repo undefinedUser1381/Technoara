64da8731fac9c26f29a72fdbbaf9b2b807797fe1
erfan <erfanghezi5050@gmail.com>
2025-01-04 16:31:12 +0330

    Add title to page

--- src/pages/mainPages/Login/LoginPage.jsx
+++ src/pages/mainPages/Login/LoginPage.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Login from "../../../components/allComponents/Auth/Login/Login"
+import useDocumentTitle from '@/hooks/useDocumentTitle'
 
 export default function LoginPage() {
+
+  useDocumentTitle("تکنو آرا | ورود")
+
   return (
     <Login />
   )
