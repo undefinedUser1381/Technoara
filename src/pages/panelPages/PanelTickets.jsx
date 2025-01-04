a591db79dde6d813eb9460bfe59150e20fed5338
erfan <erfanghezi5050@gmail.com>
2025-01-04 16:29:46 +0330

    Add title to page

--- src/pages/panelPages/PanelTickets.jsx
+++ src/pages/panelPages/PanelTickets.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import PanelTicket from '@/components/panelComponents/PanelTicket/PanelTicket'
+import useDocumentTitle from '@/hooks/useDocumentTitle'
 
 export default function PanelTickets() {
+
+  useDocumentTitle("تکنوآرا | تیکت ها")
+
   return ( <PanelTicket /> )
 }
