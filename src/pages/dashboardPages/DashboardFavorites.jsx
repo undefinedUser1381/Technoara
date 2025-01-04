760b851f1a86091c12c257982588bc090d57f837
erfan <erfanghezi5050@gmail.com>
2025-01-04 16:26:39 +0330

    Add title to page

--- src/pages/dashboardPages/DashboardFavorites.jsx
+++ src/pages/dashboardPages/DashboardFavorites.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Favorites from '../../components/dashboardComponents/Favorites/Favorites'
+import useDocumentTitle from '@/hooks/useDocumentTitle'
 
 export default function DashboardFavorites() {
+
+  useDocumentTitle("تکنو آرا | علاقه مندی ها")
+
   return (
     <>
      <Favorites />
