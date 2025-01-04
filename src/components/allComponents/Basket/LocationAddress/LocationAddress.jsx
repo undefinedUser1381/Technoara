acf12bdd30e07bd12876308619751cb17e9ae819
erfan <erfanghezi5050@gmail.com>
2025-01-04 16:24:44 +0330

    Add title to page

--- src/components/allComponents/Basket/LocationAddress/LocationAddress.jsx
+++ src/components/allComponents/Basket/LocationAddress/LocationAddress.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { useContext } from "react";
 import TopSection from "./TopSection/TopSection";
 import BottomSection from "./BottomSection/BottomSection";
+import dataContext from '@/context/dataContext/dataContext';
 
 export default function LocationAddress() {
+
+  const dataBasket = useContext(dataContext)
+
+  console.log(dataBasket.basketData.postWay , "sas");
+
   console.log("location address");
 
   return (
