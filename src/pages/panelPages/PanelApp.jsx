92c5ecf15e7ae4f1105277720400090a562e153e
erfan <erfanghezi5050@gmail.com>
2025-01-04 16:28:51 +0330

    Add title to page

--- src/pages/panelPages/PanelApp.jsx
+++ src/pages/panelPages/PanelApp.jsx
@@ -1,6 +1,10 @@
+import useDocumentTitle from "@/hooks/useDocumentTitle"
 import PanelDashboard from "../../components/panelComponents/PanelDashboard/PanelDashboard"
 
 export default function PanelApp() {
+
+  useDocumentTitle("تکنوآرا | داشبورد")
+
   return (
     <PanelDashboard />
   )
