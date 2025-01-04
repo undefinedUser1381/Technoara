dcaa8ba70adda8aa675b358e33619f348c474a26
erfan <erfanghezi5050@gmail.com>
2025-01-04 16:30:45 +0330

    Add title to page

--- src/pages/panelPages/PanelCategory.jsx
+++ src/pages/panelPages/PanelCategory.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import PanelCateogry from '@/components/panelComponents/PanelCategory/PanelCategory'
+import useDocumentTitle from '@/hooks/useDocumentTitle'
 
 export default function PanelCategory() {
+
+  useDocumentTitle("تکنوآرا | دسته بندی")
+
   return (
     <><PanelCateogry /></>
   )
