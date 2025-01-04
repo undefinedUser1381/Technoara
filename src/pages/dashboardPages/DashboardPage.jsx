9f5678a8244271be1240f5f97b72c0a2e14368c6
erfan <erfanghezi5050@gmail.com>
2025-01-04 16:27:50 +0330

    Add title to page

--- src/pages/dashboardPages/DashboardPage.jsx
+++ src/pages/dashboardPages/DashboardPage.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Dashboard from "../../components/dashboardComponents/Dashboard/Dashboard"
+import useDocumentTitle from '@/hooks/useDocumentTitle'
 
 export default function DashboardPage() {
+
+  useDocumentTitle("تکنو آرا | داشبورد")
+
   return (
     <Dashboard />
   )
