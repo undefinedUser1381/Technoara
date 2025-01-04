a2bcc0bb5b41e35a095f5d3cb7edd768a9faba8c
erfan <erfanghezi5050@gmail.com>
2025-01-04 16:30:32 +0330

    Add title to page

--- src/pages/panelPages/PanelNotifications.jsx
+++ src/pages/panelPages/PanelNotifications.jsx
@@ -1,7 +1,11 @@
 import PanelNotifs from '@/components/panelComponents/PanelNotifs/PanelNotifs'
+import useDocumentTitle from '@/hooks/useDocumentTitle'
 import React from 'react'
 
 export default function PanelNotifications() {
+
+  useDocumentTitle("تکنوآرا | پیام ها")
+
   return (
     <><PanelNotifs /></>
   )
