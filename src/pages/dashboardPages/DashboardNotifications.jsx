f972009f3b1cb9a57e38a7043640d7d528746467
erfan <erfanghezi5050@gmail.com>
2025-01-04 16:26:54 +0330

    Add title to page

--- src/pages/dashboardPages/DashboardNotifications.jsx
+++ src/pages/dashboardPages/DashboardNotifications.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Notification from '../../components/dashboardComponents/Notification/Notification'
+import useDocumentTitle from '@/hooks/useDocumentTitle'
 
 export default function DashboardNotifications() {
+
+  useDocumentTitle("تکنو آرا | پیام ها")
+
   return (
     <>
       <Notification />
