691105d98128fb0c65b654beb1b8b937ae64bcf8
erfan <erfanghezi5050@gmail.com>
2025-01-04 16:26:26 +0330

    Add title to page

--- src/pages/dashboardPages/DashboardAddress.jsx
+++ src/pages/dashboardPages/DashboardAddress.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Address from "../../components/dashboardComponents/Address/Address"
+import useDocumentTitle from '@/hooks/useDocumentTitle'
 
 export default function DashboardAddress() {
+
+  useDocumentTitle("تکنوآرا | آدرس ها")
+
   return (
     <>
       <Address />
