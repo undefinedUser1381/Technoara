4a87600fd2980edfbd19a070dd9b9eb328b229ad
erfan <erfanghezi5050@gmail.com>
2025-01-04 16:28:07 +0330

    Add title to page

--- src/pages/panelPages/PanelUsers.jsx
+++ src/pages/panelPages/PanelUsers.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import PanelUser from "@/components/panelComponents/PanelUsers/PanelUser";
+import useDocumentTitle from "@/hooks/useDocumentTitle";
 
 export default function PanelUsers() {
+  
+  useDocumentTitle("تکنوآرا | کاربران")
+
   return (
     <>
       <PanelUser />
