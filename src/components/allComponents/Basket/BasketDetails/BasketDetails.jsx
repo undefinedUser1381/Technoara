6f612edadc482f1e386c8635ef58c4b1131a1189
erfan <erfanghezi5050@gmail.com>
2025-01-08 10:04:27 +0330

    Modified

--- src/components/allComponents/Basket/BasketDetails/BasketDetails.jsx
+++ src/components/allComponents/Basket/BasketDetails/BasketDetails.jsx
@@ -1,8 +1,13 @@
-import React , {memo} from 'react'
+import React , {memo, useContext} from 'react'
 import TopBox from './TopBox/TopBox';
 import BottomBox from './BottomBox/BottomBox';
+import dataContext from '@/context/dataContext/dataContext';
 
 const BasketDetails = memo(() => {
+
+    const dataBasket = useContext(dataContext)
+
+    console.log(dataBasket.basketData.postWay , "afa");
 
     console.log("Basket detail");
     return(
